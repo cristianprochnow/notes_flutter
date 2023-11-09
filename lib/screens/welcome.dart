9c12a78f581567d94dec82b777bf42a458f099dd
Cristian Prochnow <cristianprochnow@gmail.com>
2023-11-08 21:17:32 -0300

    feat: add skeleton for welcome page

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:notes_flutter/widgets/roundeb_button.dart';
 
 class Welcome extends StatelessWidget {
   const Welcome({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 24,
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            const Center(
+              child: Text(
+                'Notas',
+                style: TextStyle(
+                  fontSize: 56,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 32,
+            ),
+            RoundedButton(
+              title: 'Login', 
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              color: Colors.green,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
